f8f6da30ffebbb2338bd772d0f03f4e9b41b418f
Simon Kallfass <simon.kallfass@gmx.net>
2018-05-12 21:30:06 +0200

    removed commands to create template of the pproject-config-file
    these commands are unnecessary because during installation of conda-packages files inside the home-folder can not be created

--- conda-build/build.sh
+++ conda-build/build.sh
@@ -1,9 +1,3 @@
 $PYTHON setup.py install
 
 cp pproject.sh $PREFIX/bin/pproject
-if ! [ -d "$HOME/.config/pproject" ]; then
-    mkdir "$HOME/.config/pproject";
-fi
-if ! [ -e "$HOME/.config/pproject/pproject_config.yml" ]; then
-    cp ouroboros/tools/pproject/pproject_config.yml "$HOME/.config/pproject/pproject_config.yml";
-fi
